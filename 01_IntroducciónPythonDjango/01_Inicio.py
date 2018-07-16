d379ff51ea9716f19d5b775a71996ef59d52abb2
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-15 21:02:22 -0400

    Hola mundo
    Creación del hola mundo, y detalle de cosas básicas.

---
+++ 01_IntroducciónPythonDjango/01_Inicio.py
@@ -0,0 +1,42 @@
+import django
+
+print "Hola mundo"
+
+'''
+Importante: no usar tildes, ni la (n), etc.
+int: 15 (numero normal)
+float: 14.3 (decimales)
+long: 35L (entero doble tamano)
+bool: True False (siempre con la inicial mayuscula)
+str: "Hola mundo" (cadenas en doble comilla, o comilla simple, o triple)
+list: [1, [2, 'three'], 4] 
+dict: {'food': 'spam' , 'taste': 'yum'} 
+tuple: (1, 'spam', 4, 'U') (listas no modificables)
+
+Operadores aritmeticos
++	suma
+-	resta
+*	mult
+/	div
+%	rest
+**	exp
+and	y
+or  o
+not	no
+>=	mayor e igual
+==	identico (igual)
+!=	distinto
+>	estricto mayor
+'''	
+
+True == False
+
+True == True
+
+s = "sdd"
+
+print s
+
+a = 1
+b = 4
+print (a + b)
