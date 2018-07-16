5d1836bb7ee7a5fde77d00d9987406061816f4e2
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-15 22:43:09 -0400

    Funciones
    Descripción y ejemplo para varias funciones

---
+++ 01_IntroducciónPythonDjango/03_Funciones.py
@@ -0,0 +1,16 @@
+l = [3, 2, 4, 5]
+
+#Castea un tipo a otro
+print map(str, l)
+
+#Imprime un rango de 5 numeros, incluyendo el 0
+print range(5)
+
+#Imprime un rango en un intervalo indicado
+print range(5, 10)
+
+#Redondear
+print round(3.1415, 2)
+
+#Comando de ayuda para entender funciones
+help(sorted)
